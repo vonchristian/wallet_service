4747a613695ef3fe56e3fb7a67fefbc8614d8549
Von Christian Halip <vc.halip@gmail.com>
2023-08-03 23:04:23 +0800

    Enable uuid as primary key

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module WalletService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.orm(:active_record, primary_key_type: :uuid)
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
